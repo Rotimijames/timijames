f4fd43283562b8c92f9153c9827e5dcfa45cf817
Rotimijames <158163895+Rotimijames@users.noreply.github.com>
2024-09-25 09:35:14 +0100

    Add files via upload

---
+++ areaofrectangle.js
@@ -0,0 +1,9 @@
+// Function to calculate the area of a rectangle
+function rectangleArea(length, width) {
+    return length * width;
+}
+
+// Example usage
+let length = 10;
+let width = 5;
+console.log(`The area of the rectangle is: ${rectangleArea(length, width)}`);
